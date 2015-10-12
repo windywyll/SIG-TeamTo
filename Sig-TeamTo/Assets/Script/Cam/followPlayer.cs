5505133d21c5834304271f12d628fc6e7dbb7dec
unknown <w.patruno@J001-0130-02.etudiant.rubika-edu.com.local>
2015-10-12 14:16:40 +0200

    first commit

---
+++ Sig-TeamTo/Assets/Script/Cam/followPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followPlayer : MonoBehaviour {
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.transform.position= new Vector3(player.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+	}
+}
